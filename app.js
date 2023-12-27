0e676bdfd3f396ae90b1aad0c32b76f3e7b1066c
Ikhwan Nashir <ikhwan.iw22@gmail.com>
2023-12-27 23:19:14 +0700

    test restapi verceljson

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/url", (req, res, next) => {
+  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
